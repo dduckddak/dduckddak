b138701124c420622b2de45d4fbef21be89abea5
Jo-hye-won <a01051910573@gmail.com>
2024-03-06 16:11:08 +0900

    Init: 프리티어 설정

--- Ddukddak/front/ddukddak/App.tsx
+++ Ddukddak/front/ddukddak/App.tsx
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as ScreenOrientation from "expo-screen-orientation";
 
 export default function App() {
+  ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>

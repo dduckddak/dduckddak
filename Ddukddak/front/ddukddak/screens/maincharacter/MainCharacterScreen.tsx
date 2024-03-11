42b0b6af0ab8f03a90bc95171dfe544b718e2f7c
박영진 <dud_wls_95@naver.com>
2024-03-11 15:40:33 +0900

    feat: 페이지네이션

--- Ddukddak/front/ddukddak/screens/maincharacter/MainCharacterScreen.tsx
+++ Ddukddak/front/ddukddak/screens/maincharacter/MainCharacterScreen.tsx
@@ -1,24 +1,124 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+  ImageBackground,
+} from 'react-native';
 import { NavigationProp, ParamListBase } from '@react-navigation/native';
 
 interface MainCharacterScreenProps {
   navigation: NavigationProp<ParamListBase>;
 }
 
+const books = [
+  'Book Title 1',
+  'Book Title 2',
+  'Book Title 3',
+  'Book Title 4',
+  'Book Title 5',
+  'Book Title 6',
+  'Book Title 7',
+];
+
 const MainCharacterScreen: React.FC<MainCharacterScreenProps> = ({
   navigation,
 }) => {
-  const handlePress = () => {
-    navigation.navigate('detail');
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const nextPage = () => {
+    setCurrentPage((prevCurrentPage) => (prevCurrentPage + 1) % books.length);
+  };
+
+  const previousPage = () => {
+    setCurrentPage(
+      (prevCurrentPage) => (prevCurrentPage - 1 + books.length) % books.length,
+    );
   };
 
   return (
-    <View>
-      <Button title="Go" onPress={handlePress} />
-      <Text>MainCharacterScreen</Text>
-    </View>
+    <ImageBackground
+      source={require('../../assets/background.png')}
+      style={styles.imageBackground}
+    >
+      <View style={styles.flexContainer}>
+        <View style={styles.container}>
+          <Button title="<" onPress={previousPage} />
+          <View style={styles.textContainer}>
+            <TouchableOpacity onPress={() => navigation.navigate('detail')}>
+              <View style={styles.box}>
+                <Text style={styles.text}>{books[currentPage]}</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+          <Button title=">" onPress={nextPage} />
+        </View>
+        <View style={styles.dotsContainer}>
+          {books.map((_, index) => (
+            <View
+              key={index}
+              style={[
+                styles.dot,
+                index === currentPage ? styles.activeDot : styles.inactiveDot,
+              ]}
+            />
+          ))}
+        </View>
+      </View>
+    </ImageBackground>
   );
 };
+
+const styles = StyleSheet.create({
+  imageBackground: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  flexContainer: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+  },
+  textContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  box: {
+    borderWidth: 1,
+    borderColor: 'black',
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    textAlign: 'center',
+  },
+  dotsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  dot: {
+    width: 30,
+    height: 30,
+    borderRadius: 30,
+    margin: 20,
+  },
+  activeDot: {
+    backgroundColor: '#254E5A',
+  },
+  inactiveDot: {
+    backgroundColor: '#B3DABF',
+  },
+});
 
 export default MainCharacterScreen;

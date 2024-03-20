74644dfef797fc8221225471187dee9275a8b6db
이성완 <cjddktkdwk@naver.com>
2024-03-21 00:24:50 +0900

    Feat : STT_api 추가
    - APIRouter로 공통 경로 묶음
    - swagger 설정
    - stt api 추가
    - stt에 필요한 requirements 추가

--- Ddukddak/fastapi/app/main.py
+++ Ddukddak/fastapi/app/main.py
@@ -1,24 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter, File, UploadFile
 from schemas import UserPreferences, MakePhoto, ExtractPhoto  # 현재 디렉터리 내 schemas.py에서 클래스 가져오기
 from recommendations import get_recommendations
 from makephoto import make_fairytale_photo
+from stt import stt
 
-app = FastAPI()
+app = FastAPI(swagger_ui=True)
+
+# APIRouter를 사용하여 /api/v1/f 아래에 들어갈 경로를 묶음
+router = APIRouter(prefix="/api/v1/f", tags=["api"])
 
 @app.get("/")
 async def test():
     return "test"
 
-@app.post("/api/v1/f/recommendations/")
+@router.post("/recommendations/", tags=["api"])
 async def create_recommendation(preferences: UserPreferences):
     recommendations = get_recommendations(preferences.likes, preferences.dislikes, top_n=7)
     return {"recommendations": recommendations}
 
-@app.post("/api/v1/f/makephoto/")
+@router.post("/makephoto/", tags=["api"])
 async def create_maked_photo(makephoto: MakePhoto):
     return {"makephoto": makephoto}
 
-@app.post("/api/v1/f/extract-face/")
+@router.post("/extract-face/", tags=["api"])
 async def extract_face(extractphoto : ExtractPhoto):
     print("들어옴")
     result = make_fairytale_photo(extractphoto.userSeq, extractphoto.photoId)
@@ -27,3 +31,12 @@ async def extract_face(extractphoto : ExtractPhoto):
 # @app.post("/api/v1/f/test")
 # async def test_extract(extractphoto : ExtractPhoto):
 #     return upload_file(extractphoto.userSeq, extractphoto.photoId)
+
+@router.post("/stt", tags=["api"])
+async def startStt(file: UploadFile = File(...)):
+    print("STT 실행")
+    result=await stt(file)
+    return {'result': result}
+
+# 앱에 라우터 추가
+app.include_router(router)

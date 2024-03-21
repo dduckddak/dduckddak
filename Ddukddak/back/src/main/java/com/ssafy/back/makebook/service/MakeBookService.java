2f715b12814d33eecd77c63e97d071ed62b0f269
이성완 <cjddktkdwk@naver.com>
2024-03-21 21:34:51 +0900

    Feat : 생성 동화 목록 api
    - 생성 동화 목록 api 추가

--- Ddukddak/back/src/main/java/com/ssafy/back/makebook/service/MakeBookService.java
+++ Ddukddak/back/src/main/java/com/ssafy/back/makebook/service/MakeBookService.java
@@ -4,7 +4,10 @@ import org.springframework.http.ResponseEntity;
 
 import com.ssafy.back.makebook.dto.request.InsertMakeBookRequestDto;
 import com.ssafy.back.makebook.dto.response.InsertMakeBookResponseDto;
+import com.ssafy.back.makebook.dto.response.ListMakeBookResponseDto;
 
 public interface MakeBookService {
+	ResponseEntity<? super ListMakeBookResponseDto> listMakeBook();
+
 	ResponseEntity<? super InsertMakeBookResponseDto> insertMakeBook(InsertMakeBookRequestDto request);
 }

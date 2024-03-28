9d5b3aa4d5ad86f7fd70f8eb57f147bcb61f072c
강성은 <1ksy3629@naver.com>
2024-03-28 17:05:33 +0900

    Feat : 대화뚝딱 UI 수정중

--- Ddukddak/front/ddukddak/screens/maincharacter/TalkScreen.tsx
+++ Ddukddak/front/ddukddak/screens/maincharacter/TalkScreen.tsx
@@ -6,18 +6,19 @@ import {
   StyleSheet,
   StatusBar,
   Image,
-  TouchableOpacity
+  TouchableOpacity,
 } from 'react-native';
 import {
   cacheDirectory,
   writeAsStringAsync,
-  EncodingType
+  EncodingType,
 } from 'expo-file-system';
 import { RouteProp } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '../../App';
 import { getTalkDetail, sttTalk, triggerTalk } from '../../api/talkApi';
 import { Audio } from 'expo-av';
+import GreenButton from '../../components/GreenButton';
 
 type TalkScreenRouteProp = RouteProp<RootStackParamList, 'talk'>;
 type TalkScreenNavigationProp = StackNavigationProp<
@@ -32,19 +33,34 @@ interface TalkScreenProps {
 
 function TalkScreen({ route }: TalkScreenProps) {
   const bookId = route.params.bookId;
+
   const [subName, setSubName] = useState<string>();
   const [subBasic, setSubBasic] = useState<string>();
   const [subTalk, setSubTalk] = useState<string>();
+
+
+  /*
+    처음 화면에 들어왔을 때 캐릭터쪽에 말풍선이 생겨 있다. (ex. 안녕 지금부터 회원이가 빨간모자야)
+    isRecording이 false일 때 녹음 버튼을 누르면 녹음을 시작하면 isRecording이 true가 되면서 녹음모드가 시작된다.(startRecording함수 수행)
+    isRecording이 true일 때 마이크에 말을 전부 다한 뒤 녹음종료 버튼을 누르면 isRecording이 false가 되면서 stopRecording함수가 수행된다
+
+   */
+  const [isRecording, setIsRecording] = useState(false);
+  const [characterTalking, setCharacterTalking] = useState(true);
+
   const [recording, setRecording] = useState<Audio.Recording | undefined>(undefined);
+
   const [userScript, setUserScript] = useState<string | null>('');
-  const [gptScript, setGptScript] = useState<string | null>('');
+  const [characterScript, setCharacterScript] = useState<string | null>('');
 
   const loadTalk = async (bookId: number) => {
     try {
       const result = await getTalkDetail(bookId);
+      //   name 캐릭터 이름 , basic 기본 이미지, talk 말하는 이미지
       setSubName(result.subName);
       setSubBasic(result.subBasic);
       setSubTalk(result.subTalk);
+
     } catch (error) {
       console.error('load Talk :', error);
     }
@@ -56,6 +72,7 @@ function TalkScreen({ route }: TalkScreenProps) {
 
   async function startRecording() {
     const { status } = await Audio.requestPermissionsAsync();
+    setIsRecording(true)
     try {
       if (status !== 'granted') {
         console.error('권한 거절');
@@ -70,10 +87,14 @@ function TalkScreen({ route }: TalkScreenProps) {
 
     } catch (err) {
       console.error('녹음 실패', err);
+      // 실패시 레코딩 모드 변경
+      setIsRecording(false);
     }
   }
 
   async function stopRecording() {
+    setIsRecording(false);
+
     if (recording) {
       console.log('녹음 종료');
       await recording.stopAndUnloadAsync();
@@ -84,8 +105,10 @@ function TalkScreen({ route }: TalkScreenProps) {
       if (uri !== null) {
         const sttResult = await handleUploadSound(uri);
         if (sttResult != null) {
+          // 유저 스크립트를 화면에 출력( 텍스트로 변환된 유저의 대화 내용을 userScript에 담아주고 chracterTalking을 false로 바꾸면서 유저 말풍선 출력
           setUserScript(sttResult);
-          await handleDownloadSound(sttResult)
+          setCharacterTalking(false);
+          await handleDownloadSound(sttResult);
         }
       }
     } else {
@@ -98,7 +121,7 @@ function TalkScreen({ route }: TalkScreenProps) {
       uri: uri,
       type: 'audio/mp3', // 적절한 MIME 타입 지정
       name: `talkFile.mp3`, // 파일 이름 지정
-    }
+    };
 
     try {
       const response = await sttTalk(talkFile as File);
@@ -111,15 +134,17 @@ function TalkScreen({ route }: TalkScreenProps) {
   };
 
   const handleDownloadSound = async (userScript: string) => {
-    const TalkParm = {
+    const talkParams = {
       bookId: bookId,
-      userScript: userScript
-    }
+      userScript: userScript,
+    };
 
     try {
-      const response = await triggerTalk(TalkParm);
-
-      setGptScript(response.gptScript)
+      const response = await triggerTalk(talkParams);
+      // 정상적으로 답변이 전달되었을 때 캐릭터의 대화내용을 characterScript에 담아주고
+      // characterTalking을 true로 바꾸면서 유저 말풍선을 끄고 캐릭터 말풍선을 렌더링
+      setCharacterScript(response.gptScript);
+      setCharacterTalking(true);
       await playBase64Audio(response.gptVoiceFile);
 
     } catch (error) {
@@ -132,7 +157,7 @@ function TalkScreen({ route }: TalkScreenProps) {
     try {
       const tmpFilename = `${cacheDirectory}speech.mp3`;
       await writeAsStringAsync(tmpFilename, base64Audio, {
-        encoding: EncodingType.Base64
+        encoding: EncodingType.Base64,
       });
 
       // 새로운 오디오 객체 생성
@@ -156,28 +181,67 @@ function TalkScreen({ route }: TalkScreenProps) {
         source={require('../../assets/images/background/background.png')}
         style={styles.imageBackground}
       >
-        <Image
-          source={{ uri: subBasic }}
-          style={styles.bookImage}
-        />
-        <Text style={styles.bigtext}>{subName}</Text>
-        <View style={styles.innerContainer}>
-          <Text style={styles.bigtext}>{userScript}</Text>
-          <Text style={styles.bigtext}>{gptScript}</Text>
-          <TouchableOpacity
-            onPress={startRecording}
-            style={styles.buttonStyle}
+        <View
+          style={styles.imageContainer}
+        >
+          <Image
+            source={{ uri: subBasic }}
+            style={styles.characterImage}
+          />
+        </View>
+
+        <View
+          style={{
+            flex: 3,
+            // borderWidth: 3,
+          }}
+        >
+          <View
+            style={{
+              flex: 2,
+              justifyContent: 'center',
+              // borderWidth: 2,
+              // borderColor: 'green',
+            }}
           >
-            <Text style={styles.text}>말하기</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={stopRecording}
-            style={styles.buttonStyle}
+            {/* 대화 말풍선 나올 영역 시작 */}
+
+            {/* TODO 현재 임시로 Text로 구현, 나중에 말풍선 안에 텍스트가 담기게 CSS 수정해야함 */}
+            {
+              characterTalking ? (
+                <Text style={styles.bigtext}>캐릭터 스크립트 : {characterScript}</Text>
+              ) : (
+                <Text style={styles.bigtext}>유저스크립트 :{userScript}</Text>
+              )
+            }
+            {/* 대화 말풍선 나올 영역 끝 */}
+          </View>
+          <View
+            style={{
+              flex: 1,
+              // borderWidth: 2,
+              // borderColor: 'yellow',
+              justifyContent: 'center',
+            }}
           >
-            <Text style={styles.text}>말끝내기</Text>
-          </TouchableOpacity>
+            {isRecording ? (
+              <GreenButton onPress={stopRecording} content={'대화끝내기'}
+                           style={{
+                             width: '40%',
+                           }}
+              />
+            ) : (
+              <GreenButton onPress={startRecording} content={'대화하기'}
+                           style={{
+                             width: '40%',
+                           }}
+              />
+            )}
+          </View>
+
         </View>
       </ImageBackground>
+
     </View>
   );
 }
@@ -197,27 +261,25 @@ const styles = StyleSheet.create({
     flex: 1,
     resizeMode: 'contain',
     justifyContent: 'center',
-  },
-  bookImage: {
-    // position: 'absolute',
-    // bottom: 10, // 아래쪽으로 20픽셀 떨어진 위치
-    width: '60%',
-    height: '90%', // 이미지가 전체 컨테이너의 80%를 차지하도록 설정
-    resizeMode: 'cover',
-    borderRadius: 5,
-  },
-  buttonStyle: {
     flexDirection: 'row',
-    borderWidth: 3,
-    borderRadius: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  text: {
-    fontFamily: 'im-hyemin-bold',
-    fontSize:30
+
+  imageContainer: {
+    flex: 2,
+    height: '100%',
+    // borderWidth: 3,
+    // borderColor: 'blue',
+
+  },
+  characterImage: {
+    width: '100%',
+    height: '100%', // 이미지가 전체 컨테이너의 80%를 차지하도록 설정
+    resizeMode: 'contain',
+
+
   },
   bigtext: {
-    fontSize: 40
+    fontSize: 40,
+    // borderWidth: 3,
   },
 });

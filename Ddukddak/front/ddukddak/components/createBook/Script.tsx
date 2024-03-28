1bbe5e37c3e0e9654b22360546db9e0b12bafd5a
Jo-hye-won <a01051910573@gmail.com>
2024-03-28 10:19:04 +0900

    Feat: 로딩 컴포넌트 만들어둠

--- Ddukddak/front/ddukddak/components/createBook/Script.tsx
+++ Ddukddak/front/ddukddak/components/createBook/Script.tsx
@@ -1,51 +1,17 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import Loading from '../../components/Loading';
 
 const ScriptScreen = () => {
-  const [showScript, setShowScript] = useState(false);
-
-  const handleButtonPress = () => {
-    setShowScript(!showScript);
-  };
-
   return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={handleButtonPress} style={styles.button}>
-        <Text style={styles.buttonText}>Show Script</Text>
-      </TouchableOpacity>
-
-      {showScript && (
-        <View style={styles.scriptContainer}>
-          <Text style={styles.scriptText}>책만들기 사용설명서</Text>
-        </View>
-      )}
+    <View style={styles.Container}>
+      <Loading />
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  button: {
-    padding: 10,
-    backgroundColor: 'lightblue',
-    borderRadius: 5,
-  },
-  buttonText: {
-    fontSize: 16,
-  },
-  scriptContainer: {
-    marginTop: 20,
-    padding: 10,
-    backgroundColor: 'lightgray',
-    borderRadius: 5,
-  },
-  scriptText: {
-    fontSize: 18,
-  },
-});
-
 export default ScriptScreen;
+
+const styles = StyleSheet.create({
+  Container: { alignItems: 'center', justifyContent: 'center', flex: 1 },
+});

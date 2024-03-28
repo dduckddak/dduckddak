7d02a9242a29285c9ae9e9c5ae087956709de9df
김다희 <kdahui98@gmail.com>
2024-03-28 11:38:21 +0900

    fe/reviewapi
    - boolean -> Boolean
    - 수정, 삭제, 상세 api 삭제
    - 버튼 누르면 db에 리뷰 업데이트 됨

--- Ddukddak/front/ddukddak/api/bookApi.ts
+++ Ddukddak/front/ddukddak/api/bookApi.ts
@@ -27,7 +27,7 @@ export interface BookDetailData {
   book: {
     bookAuthor: string;
     bookStory: string;
-    isLike: boolean
+    isLike: Boolean
   };
 }
 
@@ -39,13 +39,17 @@ export interface LikeBookListData {
   }[];
 }
 
+export interface reviewData {
+  isLike: Boolean
+}
+
 type BookListResponse = ApiResponse & BookListData;
 
 type BookSearchResponse = ApiResponse & BookSearchData;
 
 type BookDetailResponse = ApiResponse & BookDetailData;
 
-type ReviewCreateResponse = ApiResponse;
+type ReviewCreateResponse = ApiResponse & reviewData;
 
 type LikeBookListResponse = ApiResponse & LikeBookListData;
 
@@ -160,7 +164,7 @@ export const getBookDetail = async (
 
 interface ReviewCreateParams {
   bookId: number;
-  isLike: boolean;
+  isLike: Boolean;
 }
 
 interface ApiResponse {
@@ -172,7 +176,15 @@ interface ApiResponse {
  * @remarks
  * POST 요청을 '/api/v1/books/reviews' 엔드포인트에 보냅니다. 성공시 메시지를 반환합니다.
  * @param {ReviewCreateParams} params 리뷰를 작성할 책의 ID와 좋아요 여부
- * @returns {Promise<ReviewCreateResponse>} "Success" 메시지를 반환합니다.
+ * @returns {Promise<ReviewCreateResponse>} 
+ * {"message": "insert success",
+  "like": Boolean} 
+  {"message": "update success",
+  "like": Boolean}
+  {"message": "delete success", 
+  "like": Boolean}
+  ( insert, update 인 경우는 true or false 값이 오고 , 
+  delete인 경우에는 null이 옵니다. )
  * @throws 400 "Bad request." 또는 401 "Certification failed.", 403 "RefreshToken error.", 404 "Not Found." 오류를 반환할 수 있습니다.
  */
 export const createReview = async (
@@ -192,89 +204,89 @@ export const createReview = async (
   }
 };
 
-interface ReviewUpdateParams {
-  bookId: number;
-  isLike: boolean;
-}
+// interface ReviewUpdateParams {
+//   bookId: number;
+//   isLike: Boolean;
+// }
 
-interface ReviewDetailData {
-  review: {
-    isLike: boolean;
-  };
-}
+// interface ReviewDetailData {
+//   review: {
+//     isLike: Boolean;
+//   };
+// }
 
-type ReviewDetailResponse = ApiResponse & ReviewDetailData;
+// type ReviewDetailResponse = ApiResponse & ReviewDetailData;
 
-/**
- * 리뷰 수정
- * @remarks
- * PUT 요청을 '/api/v1/books/reviews/{bookId}' 엔드포인트에 보냅니다. 성공시 메시지를 반환합니다.
- * @param {ReviewUpdateParams} params 수정할 리뷰의 책 ID와 좋아요 여부
- * @returns {Promise<ReviewCreateResponse>} "Success" 메시지를 반환합니다.
- * @throws 400 "Bad request." 또는 401 "Certification failed.", 403 "RefreshToken error.", 404 "Not Found." 오류를 반환할 수 있습니다.
- */
-export const updateReview = async (
-  params: ReviewUpdateParams,
-): Promise<ApiResponse> => {
-  try {
-    const response = await apiClient.put<ApiResponse>(
-      `/api/v1/books/reviews/${params.bookId}`,
-      params,
-    );
-    return response.data;
-  } catch (error) {
-    if (isAxiosError(error) && error.response) {
-      throw error.response.data;
-    }
-    throw error;
-  }
-};
+// /**
+//  * 리뷰 수정
+//  * @remarks
+//  * PUT 요청을 '/api/v1/books/reviews/{bookId}' 엔드포인트에 보냅니다. 성공시 메시지를 반환합니다.
+//  * @param {ReviewUpdateParams} params 수정할 리뷰의 책 ID와 좋아요 여부
+//  * @returns {Promise<ReviewCreateResponse>} "Success" 메시지를 반환합니다.
+//  * @throws 400 "Bad request." 또는 401 "Certification failed.", 403 "RefreshToken error.", 404 "Not Found." 오류를 반환할 수 있습니다.
+//  */
+// export const updateReview = async (
+//   params: ReviewUpdateParams,
+// ): Promise<ApiResponse> => {
+//   try {
+//     const response = await apiClient.put<ApiResponse>(
+//       `/api/v1/books/reviews/${params.bookId}`,
+//       params,
+//     );
+//     return response.data;
+//   } catch (error) {
+//     if (isAxiosError(error) && error.response) {
+//       throw error.response.data;
+//     }
+//     throw error;
+//   }
+// };
 
-/**
- * 리뷰 삭제
- * @remarks
- * DELETE 요청을 '/api/v1/books/reviews/{bookId}' 엔드포인트에 보냅니다. 성공시 메시지를 반환합니다.
- * @param {number} bookId 삭제할 리뷰의 책 ID
- * @returns {Promise<ApiResponse>} "Success" 메시지를 반환합니다.
- * @throws 401 "Certification failed." 또는 403 "RefreshToken error.", 404 "Not Found." 오류를 반환할 수 있습니다.
- */
-export const deleteReview = async (bookId: number): Promise<ApiResponse> => {
-  try {
-    const response = await apiClient.delete<ApiResponse>(
-      `/api/v1/books/reviews/${bookId}`,
-    );
-    return response.data;
-  } catch (error) {
-    if (isAxiosError(error) && error.response) {
-      throw error.response.data;
-    }
-    throw error;
-  }
-};
+// /**
+//  * 리뷰 삭제
+//  * @remarks
+//  * DELETE 요청을 '/api/v1/books/reviews/{bookId}' 엔드포인트에 보냅니다. 성공시 메시지를 반환합니다.
+//  * @param {number} bookId 삭제할 리뷰의 책 ID
+//  * @returns {Promise<ApiResponse>} "Success" 메시지를 반환합니다.
+//  * @throws 401 "Certification failed." 또는 403 "RefreshToken error.", 404 "Not Found." 오류를 반환할 수 있습니다.
+//  */
+// export const deleteReview = async (bookId: number): Promise<ApiResponse> => {
+//   try {
+//     const response = await apiClient.delete<ApiResponse>(
+//       `/api/v1/books/reviews/${bookId}`,
+//     );
+//     return response.data;
+//   } catch (error) {
+//     if (isAxiosError(error) && error.response) {
+//       throw error.response.data;
+//     }
+//     throw error;
+//   }
+// };
 
-/**
- * 리뷰 상세 정보 가져오기
- * @remarks
- * GET 요청을 '/api/v1/books/reviews/{bookId}' 엔드포인트에 보냅니다. 성공시 메시지와 함께 리뷰 정보를 반환합니다.
- * @param {string} bookId 상세 정보를 가져오려는 리뷰의 책 ID
- * @returns {Promise<ReviewDetailResponse>} "Success" 메시지와 함께 review를 반환합니다.
- * @throws 401 "Certification failed." 또는 403 "RefreshToken error.", 404 "Not Found." 오류를 반환할 수 있습니다.
- */
-export const getReviewDetail = async (
-  bookId: string,
-): Promise<ReviewDetailResponse> => {
-  try {
-    const response = await apiClient.get<ReviewDetailResponse>(
-      `/api/v1/books/reviews/${bookId}`,
-    );
-    return response.data;
-  } catch (error) {
-    if (isAxiosError(error) && error.response) {
-      throw error.response.data;
-    }
-    throw error;
-  }
-};
+// /**
+//  * 리뷰 상세 정보 가져오기
+//  * @remarks
+//  * GET 요청을 '/api/v1/books/reviews/{bookId}' 엔드포인트에 보냅니다. 성공시 메시지와 함께 리뷰 정보를 반환합니다.
+//  * @param {string} bookId 상세 정보를 가져오려는 리뷰의 책 ID
+//  * @returns {Promise<ReviewDetailResponse>} "Success" 메시지와 함께 review를 반환합니다.
+//  * @throws 401 "Certification failed." 또는 403 "RefreshToken error.", 404 "Not Found." 오류를 반환할 수 있습니다.
+//  */
+// export const getReviewDetail = async (
+//   bookId: string,
+// ): Promise<ReviewDetailResponse> => {
+//   try {
+//     const response = await apiClient.get<ReviewDetailResponse>(
+//       `/api/v1/books/reviews/${bookId}`,
+//     );
+//     return response.data;
+//   } catch (error) {
+//     if (isAxiosError(error) && error.response) {
+//       throw error.response.data;
+//     }
+//     throw error;
+//   }
+// };
 
 /**
  * 좋아요 한 책 목록을 가져오기 (BookList랑 데이터 값 똑같아서 그대로 사용)

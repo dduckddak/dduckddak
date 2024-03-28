60288b8ad350b7a2251bf2148efac1546222a8a2
강성은 <1ksy3629@naver.com>
2024-03-28 09:18:28 +0900

    Feat : 색칠뚝딱 터엉

--- Ddukddak/front/ddukddak/components/EmptyListComponent.tsx
+++ Ddukddak/front/ddukddak/components/EmptyListComponent.tsx
@@ -2,12 +2,23 @@ import { View, Image, StyleSheet, Dimensions } from 'react-native';
 
 const { width, height } = Dimensions.get('screen');
 
-function EmptyListComponent() {
+
+interface EmptyListComponentProps {
+  // 비율을 결정할 prop을 추가
+  // 이미지의 높이를 화면 높이의 75%로 기본값을 설정
+  imageHeightRatio?: number;
+}
+
+
+function EmptyListComponent({ imageHeightRatio = 0.75 }: EmptyListComponentProps) {
+  const imageHeight = height * imageHeightRatio;
+
+
   return (
     <View style={styles.emptyContainer}>
       <Image
         source={require('../assets/images/emptyList.png')}
-        style={styles.emptyImage}
+        style={{ ...styles.emptyImage, height: imageHeight }}
       />
     </View>
   );
